8567746cc2f82a0db15f3c7de53c1106542c44b6
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-19 21:39:13 +0100

    test: add `variableInclude` matcher
    * verifies a variable matching an expectation is in a list of variables
    
    test: correct `variableEquals` to strictly match all variables
    
    chore: move assertions to utility

--- test/globals.js
+++ test/globals.js
@@ -1,53 +1,7 @@
-const { use, expect } = require('chai');
+const { use } = require('chai');
 const { default: sinonChai } = require('sinon-chai');
 
+const { variableAssertions } = require('./assertions');
 
-function isDefined(value) {
-  return typeof value !== 'undefined';
-}
-
-function VariableEqual(chai, utils) {
-
-  utils.addMethod(chai.Assertion.prototype, 'variableEqual', function(comparison) {
-    var variables = this._obj;
-    var expectedVariables = comparison;
-
-    expectedVariables.forEach((expectedVariable) => {
-      const {
-        name,
-        type,
-        detail,
-        info,
-        scope,
-        isList,
-        origin,
-        entries
-      } = expectedVariable;
-
-      const actualVariable = variables.find(
-        v => (!isDefined(name) || v.name === name) && (!isDefined(scope) || v.scope?.id === scope)
-      );
-
-      expect(actualVariable, `variable[name=${name}, scope=${scope}]`).to.exist;
-
-      isDefined(type) && expect(actualVariable.type, `variable[name=${name}].type`).to.eql(type);
-      isDefined(info) && expect(actualVariable.info, `variable[name=${name}].info`).to.eql(info);
-      isDefined(detail) && expect(actualVariable.detail, `variable[name=${name}].detail`).to.eql(detail);
-      isDefined(scope) && expect(actualVariable.scope.id, `variable[name=${name}].scope.id`).to.eql(scope);
-      isDefined(isList) && expect(!!actualVariable.isList, `variable[name=${name}].isList`).to.eql(!!isList);
-      isDefined(entries) && expect(actualVariable.entries, `variable[name=${name}].entries`).to.variableEqual(entries);
-
-      isDefined(origin) && origin.forEach((expectedOrigin) => {
-        const foundOrigin = actualVariable.origin.find(o => o.id === expectedOrigin);
-        expect(foundOrigin, `origin[name=${expectedOrigin}]`).to.exist;
-      });
-
-      isDefined(origin) && expect(actualVariable.origin.length, `variable[name=${name}].origin.length`).to.eql(origin.length);
-    });
-
-    expect(variables.length, 'variables.length').to.eql(expectedVariables.length);
-  });
-}
-
-use(VariableEqual);
+use(variableAssertions);
 use(sinonChai);
